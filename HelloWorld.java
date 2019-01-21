78d5993065f74c87154fcf4ab594a746286c1c2a
Iokepa Lapera <Kepa@Keos-MacBook-Air.local>
2019-01-20 20:01:04 -1000

    Added method

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+        String s = "hello";
+        Greetings(s);
+    }
+    
+    public static void Greetings(String s){
+        
+        System.out.println(s);
+        
+    }
+    
+}
+
